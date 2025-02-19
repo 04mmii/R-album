b02c6275d799486f2738be1b505d4897541448c6
04mmii <04mmii@naver.com>
2025-02-19 12:07:31 +0900

    푸터 부분 수정

--- src/components/common/footer/CommonFooter.tsx
+++ src/components/common/footer/CommonFooter.tsx
@@ -1,7 +1,7 @@
 import { useRecoilState, useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { imageData } from "@/recoil/selectors/imageSelectors";
 import { pageState } from "@/recoil/atoms/pageState";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { searchState } from "@/recoil/atoms/searchState";
 import styles from "./CommonFooter.module.scss";
 
@@ -9,89 +9,84 @@ function CommonFooter() {
   const imgSelector = useRecoilValueLoadable(imageData);
   const search = useRecoilValue(searchState);
   const [page, setPage] = useRecoilState(pageState);
-  const [step, setStep] = useState(0);
+  const [currentGroup, setCurrentGroup] = useState(0);
 
   useEffect(() => {
-    setStep(0);
+    setCurrentGroup(0);
   }, [search]);
 
-  // 페이지 리스트 UI 생성
-  const newArr: number[] = new Array();
-  for (let i = 1; i <= imgSelector.contents.total_page; i++) {
-    newArr.push(i);
-  }
-  const length = newArr.length;
-  const divide =
-    Math.floor(length / 10) + (Math.floor(length % 10) > 0 ? 1 : 0);
-  const res = [];
+  const paginationData = useMemo(() => {
+    if (imgSelector.state !== "hasValue")
+      return { totalPages: 0, pageGroups: [] };
 
-  for (let i = 0; i <= divide; i++) {
-    //배열 0부터 n개씩 잘라 새 배열에 넣기
-    res.push(newArr.splice(0, 10));
-  }
+    const totalPages = imgSelector.contents.total_pages || 0;
+    const pageGroups = [];
+    for (let i = 0; i < totalPages; i += 10) {
+      pageGroups.push(
+        Array.from(
+          { length: Math.min(10, totalPages - i) },
+          (_, index) => i + index + 1
+        )
+      );
+    }
+    return { totalPages, pageGroups };
+  }, [imgSelector]);
 
-  // -------------------------
+  useEffect(() => {
+    if (paginationData.totalPages > 0) {
+      setCurrentGroup(Math.floor((page - 1) / 10));
+    }
+  }, [page, paginationData.totalPages]);
 
   const moveToPage = (selected: number) => {
     setPage(selected);
+    setCurrentGroup(Math.floor((selected - 1) / 10));
   };
 
   const moveToPrev = () => {
-    if (step === 0) return;
-    else {
-      setStep(step - 1);
-      setPage(res[step - 1][0]);
+    if (currentGroup > 0) {
+      setCurrentGroup((prev) => prev - 1);
+      setPage(paginationData.pageGroups[currentGroup - 1][0]);
     }
   };
 
   const moveToNext = () => {
-    if (step < res[step].length - 2) {
-      setStep(step + 1);
-      setPage(res[step + 1][0]);
-    } else return;
+    if (currentGroup < paginationData.pageGroups.length - 1) {
+      setCurrentGroup((prev) => prev + 1);
+      setPage(paginationData.pageGroups[currentGroup + 1][0]);
+    }
   };
+
+  if (paginationData.totalPages === 0) return null;
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.pagenation}>
-        <button className={styles.pagenation__button} onClick={moveToPrev}>
+      <div className={styles.pagination}>
+        <button
+          className={styles.pagination__button}
+          onClick={moveToPrev}
+          disabled={currentGroup === 0}
+        >
           <img src="src/assets/icons/icon-arrowLeft.svg" alt="왼쪽화살표" />
         </button>
-        {/* //변경될 UI부분 */}
-        {/* <span>1</span> */}
-        {res[step] &&
-          res[step].map((item: number, index: number) => {
-            if (item < 11) {
-              return (
-                <button
-                  className={
-                    index === page - 1
-                      ? `${styles.pagenation__button} ${styles.active}`
-                      : `${styles.pagenation__button} ${styles.inactive}`
-                  }
-                  key={item}
-                  onClick={() => moveToPage(item)}
-                >
-                  {item}
-                </button>
-              );
-            } else {
-              return (
-                <button
-                  className={
-                    index === page - 1 - step * 10
-                      ? `${styles.pagenation__button} ${styles.active}`
-                      : `${styles.pagenation__button} ${styles.inactive}`
-                  }
-                  key={item}
-                  onClick={() => moveToPage(item)}
-                >
-                  {item}
-                </button>
-              );
-            }
-          })}
-        <button className={styles.pagenation__button} onClick={moveToNext}>
+
+        {paginationData.pageGroups[currentGroup]?.map((item: number) => (
+          <button
+            className={`${styles.pagination__button} ${
+              page === item ? styles.active : styles.inactive
+            }`}
+            key={item}
+            onClick={() => moveToPage(item)}
+          >
+            {item}
+          </button>
+        ))}
+
+        <button
+          className={styles.pagination__button}
+          onClick={moveToNext}
+          disabled={currentGroup === paginationData.pageGroups.length - 1}
+        >
           <img src="src/assets/icons/icon-arrowRight.svg" alt="오른쪽화살표" />
         </button>
       </div>
